fb9edd3437ed512253c3c8c451c18dd3358bad19
Katomatsu <adogairbekov@gmail.com>
2022-10-16 16:06:38 +0400

    maked timer on the site

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-window.addEventListener('DOMContentLoaded', () => {
+'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+    // TABS
     const tabContent = document.querySelectorAll('.tabcontent'),
           tabs = document.querySelectorAll('.tabheader__item'),
           tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
         tabContent.forEach(item => {
-            item.classList.add('hide')
-            item.classList.remove('show', 'fade')
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
 
         tabs.forEach(item => {
@@ -34,5 +36,62 @@ window.addEventListener('DOMContentLoaded', () => {
                 }
             });
         }
-    })
+    });
+
+    //TIMER
+
+    const deadline = '2023-06-15';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60 ),
+              seconds = Math.floor((t / 1000) % 60 );
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    } 
+
+    function getZero(num) {
+        if (num <= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateCLock, 1000);
+        updateCLock();
+        
+        function updateCLock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                days.innerHTML = `00`;
+                hours.innerHTML =  `00`;
+                minutes.innerHTML =  `00`;
+                seconds.innerHTML =  `00`;
+
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
